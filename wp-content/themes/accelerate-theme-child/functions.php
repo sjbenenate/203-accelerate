fdb51df154876f8506518be0624e98c8c2a0f3e9
sjbenenate <sarahjoybenenate@gmail.com>
2018-10-27 20:53:21 -0700

    social media menu made round icons

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,7 +16,11 @@
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
-}
+  /*wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/5.4.2/css/font-awesome.min.css' );*/
+ /* wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/font-awesome/fontawesome.min.css' );*/
+  /*wp_enqueue_style( 'font-awesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );*/
+  wp_enqueue_style( 'font-awesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+  }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
 

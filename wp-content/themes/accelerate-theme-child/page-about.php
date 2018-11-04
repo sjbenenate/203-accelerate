6d64f8fd1bb2ded9a1f38879c4be6d5c097d217e
sjbenenate <sarahjoybenenate@gmail.com>
2018-11-04 00:05:10 -0700

    about-page done the long, repeated way

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -11,66 +11,68 @@ get_header(); ?>
 
 	<div class="site-content">
 		<div class="main-content" role="main">
-      <h3><?php echo get_field('intro_title') ?></h3>
-      <p><?php echo get_field('intro_paragraph') ?></p>
+      <div class="about-intro">
+        <h3><?php echo get_field('intro_title') ?></h3>
+        <p><?php echo get_field('intro_paragraph') ?></p>
+      </div>
       
       <!-- About page article blurbs, did not use custom post 
       types here for ease of updating in a single page location 
       in control dashboard -->
-      <article class="icon-left-third">
-        <div class="about-article-aside">
-          <h2><?php echo get_field('section_title_1'); ?></h2>
-          <p><?php echo get_field('section_paragraph_1'); ?></p>
-        </div>
-        <figure class="about-icon">
-          <?php $icon = get_field('section_icon_1'); 
-          if( $icon ) {
-            echo wp_get_attachment_image( $icon, $size );
-          } ?>
-        </figure>
-      </article>
-      
-
-      <article class="icon-right-third">
-        <div class="about-article-aside">
-          <h2><?php echo get_field('section_title_2'); ?></h2>
-          <p><?php echo get_field('section_paragraph_2'); ?></p>
-        </div>
-        <figure class="about-icon">
-          <?php $icon = get_field('section_icon_2'); 
-          if( $icon ) {
-            echo wp_get_attachment_image( $icon, $size );
-          } ?>
-        </figure>
-      </article>
-      
-      <article class="icon-left-third">
-        <div class="about-article-aside">
-          <h2><?php echo get_field('section_title_3'); ?></h2>
-          <p><?php echo get_field('section_paragraph_3'); ?></p>
-        </div>
-        <figure class="about-icon">
-          <?php $icon = get_field('section_icon_3'); 
-          if( $icon ) {
-            echo wp_get_attachment_image( $icon, $size );
-          } ?>
-        </figure>
-      </article>
-      
-      <article class="icon-right-third">
-        <div class="about-article-aside">
-          <h2><?php echo get_field('section_title_4'); ?></h2>
-          <p><?php echo get_field('section_paragraph_4'); ?></p>
-        </div>
-        <figure class="about-icon">
-          <?php $icon = get_field('section_icon_4'); 
-          if( $icon ) {
-            echo wp_get_attachment_image( $icon, $size );
-          } ?>
-        </figure>
-      </article>
+      <div class="about-article-wrapper">
+        <article class="about-article">
+          <div class="about-article-aside right">
+            <h2><?php echo get_field('section_title_1'); ?></h2>
+            <p><?php echo get_field('section_paragraph_1'); ?></p>
+          </div>
+          <figure class="about-icon left">
+            <?php $icon = get_field('section_icon_1'); 
+            if( $icon ) {
+              echo wp_get_attachment_image( $icon, $size );
+            } ?>
+          </figure>
+        </article>
         
-          
+
+        <article class="about-article">
+          <div class="about-article-aside left">
+            <h2><?php echo get_field('section_title_2'); ?></h2>
+            <p><?php echo get_field('section_paragraph_2'); ?></p>
+          </div>
+          <figure class="about-icon right">
+            <?php $icon = get_field('section_icon_2'); 
+            if( $icon ) {
+              echo wp_get_attachment_image( $icon, $size );
+            } ?>
+          </figure>
+        </article>
+        
+        <article class="about-article">
+          <div class="about-article-aside right">
+            <h2><?php echo get_field('section_title_3'); ?></h2>
+            <p><?php echo get_field('section_paragraph_3'); ?></p>
+          </div>
+          <figure class="about-icon left">
+            <?php $icon = get_field('section_icon_3'); 
+            if( $icon ) {
+              echo wp_get_attachment_image( $icon, $size );
+            } ?>
+          </figure>
+        </article>
+        
+        <article class="about-article">
+          <div class="about-article-aside left">
+            <h2><?php echo get_field('section_title_4'); ?></h2>
+            <p><?php echo get_field('section_paragraph_4'); ?></p>
+          </div>
+          <figure class="about-icon right">
+            <?php $icon = get_field('section_icon_4'); 
+            if( $icon ) {
+              echo wp_get_attachment_image( $icon, $size );
+            } ?>
+          </figure>
+        </article>
+      </div><!-- about article wrapper -->
     </div><!-- .main-content -->
 
 
